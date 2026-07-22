b6b3d7e134865023f3f18a86e975c4c208612a41
m-sameerkhan <sameerbalouch758@gmail.com>
2026-07-22 09:37:28 -0700

    Fix trailing whitespace in API_BASE_URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,2 @@
-window.API_BASE_URL = "https://pipeline-fares-cellular-con.trycloudflare.com";
+// window.API_BASE_URL = "https://pipeline-fares-cellular-con.trycloudflare.com";
+window.API_BASE_URL = "http://localhost:8000";
